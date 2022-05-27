e19171e4883fcee15812bf1f10eb5510de018969
Sergio Centenera <scentmay@gmail.com>
2022-05-27 11:32:44 +0200

    Commit final

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
- //Cuando carga la página iniciamos la siguiente función automáticamente
- 
+ //Cuando carga la página iniciamos la siguiente función automáticamente (.onload)
+  
  window.onload = () => {
-    let randomNumber = Math.random() *10;
-    document.querySelector("#excuse").innerHTML = generateExcuse();
-    console.log("Hola! " + randomNumber);
- };
+    document.querySelector("#btn").addEventListener("click", () => {
+        document.querySelector("#excuse").innerHTML = generateExcuse();
+ 
+    });
+ }
 
  let generateExcuse = () => {
 
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-    return "My dog eat my homework";
- }
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    return who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
+ };
